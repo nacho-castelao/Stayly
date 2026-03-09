41e261c4a331de65316eff9e7e5564e088c9d5ca
Nacho Castelao <nachocastelao@outlook.es>
2026-03-09 13:07:35 +0100

    Add dotenv support and load env variables in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 require_once '../core/Autoloader.php'; 
 require_once '../core/Config.php';
 

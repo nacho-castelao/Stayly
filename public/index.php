c3485a15846fa238b9284490fd333b983264f0fd
Nacho Castelao <nachocastelao@outlook.es>
2026-03-09 13:13:13 +0100

    Fix .env loading path in index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 require_once '../core/Autoloader.php'; 
 require_once '../core/Config.php';

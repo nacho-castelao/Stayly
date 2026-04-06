b2be8e3facdb9189efdb65bb1da23061b299b25e
Nacho <nacho3339@gmail.com>
2026-04-06 08:47:27 +0200

    feat: password toggle + delete modal logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,37 @@ if (userIcon && dropdown) {
     }
   });
 }
+
+/* Toggle password visibility */
+document.querySelectorAll(".toggle-pw").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const input = document.getElementById(btn.dataset.target);
+    if (!input) return;
+    input.type = input.type === "password" ? "text" : "password";
+    btn.querySelector(".eye-icon").style.opacity =
+      input.type === "text" ? "0.5" : "1";
+  });
+});
+
+// Delete Account (modal logic)
+const deleteAccountform = document.getElementById("delete-form");
+const modal = document.getElementById("deleteModal");
+const formButton = document.querySelector("#confirmDelete");
+
+deleteAccountform.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop instant submit
+  modal.classList.remove("hidden"); // show modal
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+  }
+});
+
+deleteAccountform.addEventListener('click', () => {
+  form.submit();
+});
+
+
+

e1c9b9530c9f9eca684f411d51cc27a97366b9ed
Nacho Castelao <nachocastelao@outlook.es>
2026-04-08 09:36:53 +0200

    style: style: apply PSR-12 formatting to "index.php"

--- public/index.php
+++ public/index.php
@@ -8,12 +8,13 @@ require_once BASE_PATH . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
-require_once BASE_PATH . '/core/Autoloader.php'; 
+require_once BASE_PATH . '/core/Autoloader.php';
 require_once BASE_PATH . '/core/Config.php';
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
-function loadDefault(){
+function loadDefault()
+{
     $controller = DEFAULT_CONTROLLER;
     $action = DEFAULT_ACTION;
 
@@ -21,23 +22,19 @@ function loadDefault(){
     $obj->$action();
 }
 
-if(isset($_GET['controller']) && preg_match("/^[A-Za-z][A-Za-z0-9_]*$/",$_GET['controller'])){
-    $controller = $_GET['controller'].'Controller';
+if (isset($_GET['controller']) && preg_match("/^[A-Za-z][A-Za-z0-9_]*$/", $_GET['controller'])) {
+    $controller = $_GET['controller'] . 'Controller';
 
-    if(class_exists($controller) && method_exists($controller,$action)){
+    if (class_exists($controller) && method_exists($controller, $action)) {
         $obj = new $controller();
         $obj->$action();
-
-    }elseif(class_exists($controller) && !method_exists($controller,$action)){
+    } elseif (class_exists($controller) && !method_exists($controller, $action)) {
         $obj = new $controller();
         $action = DEFAULT_ACTION;
         $obj->$action();
-        
-    }else{
+    } else {
         loadDefault();
     }
-}else{
+} else {
     loadDefault();
 }
-
-?>

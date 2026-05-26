8229f1007222578b334afbdfe419053d302c498d
Nacho Castelao <nachocastelao@outlook.es>
2026-05-26 13:33:22 +0200

    fix: prevent null errors in delete modal handlers

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,17 +3,17 @@ const deleteAccountform = document.getElementById("delete-form");
 const modal = document.getElementById("deleteModal");
 const formButton = document.querySelector("#confirmDelete");
 
-deleteAccountform.addEventListener("submit", function (e) {
+deleteAccountform?.addEventListener("submit", function (e) {
   e.preventDefault(); // stop instant submit
   modal.classList.remove("hidden"); // show modal
 });
 
-modal.addEventListener("click", (e) => {
+modal?.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.classList.add("hidden");
   }
 });
 
-deleteAccountform.addEventListener("click", () => {
+deleteAccountform?.addEventListener("click", () => {
   form.submit();
 });
